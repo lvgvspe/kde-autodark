0df376a4747eef767508dd311aa604e52fe6097c
cavaros <164836690+cavaros@users.noreply.github.com>
2024-03-27 21:12:59 -0300

    Create .autodark.sh

---
+++ .autodark.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+darktime=$(date -d "17:30" +"%s")
+littime=$(date -d "05:30" +"%s")
+while true; do
+        now=$(date +"%s")
+        if [ $now -gt $darktime ]; then
+                /usr/bin/lookandfeeltool -a 'org.kde.breezedark.desktop'
+                sec=$(( $(date -d "23:59:59" +"%s") - $now ))
+                sectotime=$(date -u -d @${sec} +"%Hh%Mm%Ss")
+                echo "Sleeping for $sectotime"
+                sleep $sec
+        elif [ $now -lt $littime ]; then
+                /usr/bin/lookandfeeltool -a 'org.kde.breezedark.desktop'
+                sec=$(( $littime - $now ))
+                sectotime=$(date -u -d @${sec} +"%Hh%Mm%Ss")
+                echo "Sleeping for $sectotime"
+                sleep $sec
+        else
+                /usr/bin/lookandfeeltool -a 'org.kde.breeze.desktop'
+                sec=$(( $darktime - $now ))
+                sectotime=$(date -u -d @${sec} +"%Hh%Mm%Ss")
+                echo "Sleeping for $sectotime"
+                sleep $sec
+        fi
+done

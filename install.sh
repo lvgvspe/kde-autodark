ececb14b9f5bd570aa45ff3d8db80bba95f6de32
cavaros <lucas.camargobatista@icloud.com>
2024-04-29 12:36:36 -0300

    fix: fixed bad commands

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@ chmod +x .autodark.sh
 sudo cp autodark.service /etc/systemd/user
 sudo cp resumedark.service /etc/systemd/user
 sudo cp suspend@.service /etc/systemd/system
-sudo systemctl daemon-reload && sudo systemctl enable suspend@$(whoami)
-sudo cp suspend.target ~/.config/user
+sudo systemctl daemon-reload
+sudo systemctl enable suspend@$(whoami)
+mkdir -p ~/.config/systemd/user
+cp suspend.target ~/.config/systemd/user
 systemctl --user daemon-reload
 systemctl --user enable autodark
 systemctl --user enable resumedark
